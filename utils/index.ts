1c90e7e50452a166cbe162a32dc21b914db0f555
prasadk <prasadkatepk@gmail.com>
2024-10-27 23:44:37 +0530

    chore: add util function to covert number into currency

---
+++ utils/index.ts
@@ -0,0 +1,12 @@
+export function formatPrice(
+  amount: number,
+  locale: string = "en-US",
+  currency: string = "USD"
+): string {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+}
