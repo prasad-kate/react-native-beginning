b768dfeaabeaceb9035b1694378ec232fe561168
prasad-kate <prasadkatepk@gmail.com>
2024-10-29 18:58:38 +0530

    fix: back button navigation on order details screen

--- components/DetailsScreens/OrderDetailsHeader.tsx
+++ components/DetailsScreens/OrderDetailsHeader.tsx
@@ -1,9 +1,11 @@
 import { detailsScreenStyles } from "@/styles/detailsScreen.styles";
 import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 const OrderDetailsHeader = () => {
+  const router = useRouter();
   return (
     <View style={detailsScreenStyles.headerContainer}>
       <Text style={detailsScreenStyles.headerText}>My Orders</Text>
@@ -12,6 +14,9 @@ const OrderDetailsHeader = () => {
         size={24}
         color="gray"
         style={detailsScreenStyles.backIcon}
+        onPress={() => {
+          router.back();
+        }}
       />
     </View>
   );
