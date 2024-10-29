dce3b7253a2bde6b9d6ffdcb01a8816980136747
prasad-kate <prasadkatepk@gmail.com>
2024-10-29 13:01:21 +0530

    refactor: changed map to flatlist in notifications list

--- components/NotificationScreen/NotificationsCardSection.tsx
+++ components/NotificationScreen/NotificationsCardSection.tsx
@@ -1,22 +1,24 @@
 import { notificationsList } from "@/constants/notificationScreenConstants";
 import React from "react";
-import { ScrollView } from "react-native";
+import { FlatList } from "react-native";
 import NotificationCard from "./NotificationCard";
 
 const NotificationsCardSection = () => {
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      {notificationsList?.map(({ title, description, image }, index) => {
+    <FlatList
+      data={notificationsList}
+      keyExtractor={(_, index) => `${index}`}
+      renderItem={({ item }) => {
         return (
           <NotificationCard
-            key={index}
-            title={title}
-            description={description}
-            image={image}
+            title={item.title}
+            description={item.description}
+            image={item.image}
           />
         );
-      })}
-    </ScrollView>
+      }}
+      showsVerticalScrollIndicator={false}
+    />
   );
 };
 
