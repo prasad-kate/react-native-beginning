b1a48a3968ef09e0aca2ecaea5cad719e4bc9f00
prasadk <prasadkatepk@gmail.com>
2024-11-03 20:39:59 +0530

    feat: add settings screen header

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import SettingsScreenHeader from "@/components/Settings/SettingsScreenHeader";
 import React from "react";
+import { View } from "react-native";
 
 const Settings = () => {
   return (
     <View>
-      <Text>Settings</Text>
+      <SettingsScreenHeader />
     </View>
   );
 };
