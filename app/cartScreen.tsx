59f44493e0b82ebc7a86769b00031c397a4ee8d8
prasadk <prasadkatepk@gmail.com>
2024-11-06 23:54:15 +0530

    style: cartScreen parent container spacing

--- app/cartScreen.tsx
+++ app/cartScreen.tsx
@@ -1,10 +1,11 @@
 import CartScreenHeader from "@/components/CartScreen/CartScreenHeader";
+import { detailsScreenStyles } from "@/styles/detailsScreen.styles";
 import React from "react";
 import { View } from "react-native";
 
 const cartScreen = () => {
   return (
-    <View>
+    <View style={detailsScreenStyles.container}>
       <CartScreenHeader />
     </View>
   );
