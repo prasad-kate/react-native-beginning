aa46ca2c0247bc193c6e690adb33ad817152bf90
prasad-kate <prasadkatepk@gmail.com>
2024-11-16 13:29:14 +0530

    fix: back navigation correction in common back button header

--- components/BackButtonHeader.tsx
+++ components/BackButtonHeader.tsx
@@ -1,14 +1,32 @@
 import { detailsScreenStyles } from "@/styles/detailsScreen.styles";
 import { BackButtonHeaderProps } from "@/types";
 import { Ionicons } from "@expo/vector-icons";
-import { Href, useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import { Href, useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { BackHandler, Text, View } from "react-native";
 
 const BackButtonHeader = ({
   title,
   backNavigationPath,
 }: BackButtonHeaderProps) => {
   const router = useRouter();
+
+  const { preventBackNavigation } = useLocalSearchParams();
+
+  // to prevent navigation back to success page after redirection
+  useEffect(() => {
+    if (preventBackNavigation === "true") {
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        () => {
+          router.push(backNavigationPath as Href);
+          return true;
+        }
+      );
+
+      return () => backHandler.remove();
+    }
+  }, []);
 
   return (
     <View style={detailsScreenStyles.shippingDetailsHeaderContainer}>
