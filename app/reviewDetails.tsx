0b6f832aa5527190e7a9858c798a79b61515819a
prasad-kate <prasadkatepk@gmail.com>
2024-11-16 13:23:59 +0530

    refactor: change header to common header in review details screen

--- app/reviewDetails.tsx
+++ app/reviewDetails.tsx
@@ -1,13 +1,12 @@
-import ReviewDetailsHeader from "@/components/ReviewDetails/ReviewDetailsHeader";
+import BackButtonHeader from "@/components/BackButtonHeader";
 import ReviewDetailsSection from "@/components/ReviewDetails/ReviewDetailsSection";
 import { detailsScreenStyles } from "@/styles/detailsScreen.styles";
-import React from "react";
 import { View } from "react-native";
 
 const ReviewDetails = () => {
   return (
     <View style={detailsScreenStyles.container}>
-      <ReviewDetailsHeader />
+      <BackButtonHeader title="My Reviews" />
       <ReviewDetailsSection />
     </View>
   );
