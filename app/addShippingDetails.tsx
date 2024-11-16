2f09f281b495e8461acd06ed184dc543df1b336e
prasad-kate <prasadkatepk@gmail.com>
2024-11-16 13:15:02 +0530

    refactor: changed header to common header in add-shipping-details screen

--- app/addShippingDetails.tsx
+++ app/addShippingDetails.tsx
@@ -1,12 +1,12 @@
+import BackButtonHeader from "@/components/BackButtonHeader";
 import AddShippingDetailsForm from "@/components/DetailsScreens/shippingDetails/AddShippingDetailsForm";
-import AddShippingDetailsHeader from "@/components/DetailsScreens/shippingDetails/AddShippingDetailsHeader";
 import { detailsScreenStyles } from "@/styles/detailsScreen.styles";
 import { View } from "react-native";
 
 const AddShippingDetails = () => {
   return (
     <View style={detailsScreenStyles.container}>
-      <AddShippingDetailsHeader />
+      <BackButtonHeader title="Add Shipping Address" />
       <AddShippingDetailsForm />
     </View>
   );
