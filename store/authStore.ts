3a32db04c11d2ffdaf5ffc346eb22c153714ae0a
prasadk <prasadkatepk@gmail.com>
2024-11-23 13:17:15 +0530

    feat: setup zustand

---
+++ store/authStore.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface AuthState {
+  authToken: string | null;
+  setAuthToken: (authToken: string) => void;
+  clearAuthToken: () => void;
+}
+
+const authStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      authToken: null,
+      setAuthToken: (authToken) => set({ authToken }),
+      clearAuthToken: () => set({ authToken: null }),
+    }),
+    {
+      name: "auth-storage", // storage key
+    }
+  )
+);
+
+export default authStore;
