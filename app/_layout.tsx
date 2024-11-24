488c2cb8dd6c427e9ee68ec9de23f25117d7774b
prasadk <prasadkatepk@gmail.com>
2024-11-24 20:07:59 +0530

    feat: setup tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { uiStyles } from "@/styles/global.styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
@@ -9,6 +10,8 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 preventAutoHideAsync();
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -26,66 +29,74 @@ export default function RootLayout() {
   }
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={uiStyles.container}>
-        <StatusBar barStyle="dark-content" backgroundColor={"white"} />
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="registerUser/index"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="loginUser/index"
-            options={{ headerShown: false }}
-          />
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <SafeAreaView style={uiStyles.container}>
+          <StatusBar barStyle="dark-content" backgroundColor={"white"} />
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="registerUser/index"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="loginUser/index"
+              options={{ headerShown: false }}
+            />
 
-          {/* tabs */}
-          <Stack.Screen name="tabs" options={{ headerShown: false }} />
+            {/* tabs */}
+            <Stack.Screen name="tabs" options={{ headerShown: false }} />
 
-          {/* single product screen */}
-          <Stack.Screen
-            name="singleProductScreen"
-            options={{ headerShown: false }}
-          />
+            {/* single product screen */}
+            <Stack.Screen
+              name="singleProductScreen"
+              options={{ headerShown: false }}
+            />
 
-          {/* details screens */}
-          <Stack.Screen name="orderDetails" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="paymentDetails"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="shippingDetails"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="reviewDetails" options={{ headerShown: false }} />
-          <Stack.Screen name="settings" options={{ headerShown: false }} />
+            {/* details screens */}
+            <Stack.Screen
+              name="orderDetails"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="paymentDetails"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="shippingDetails"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="reviewDetails"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="settings" options={{ headerShown: false }} />
 
-          {/* add details screens */}
-          <Stack.Screen
-            name="addShippingDetails"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="addPaymentDetails"
-            options={{ headerShown: false }}
-          />
+            {/* add details screens */}
+            <Stack.Screen
+              name="addShippingDetails"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="addPaymentDetails"
+              options={{ headerShown: false }}
+            />
 
-          {/* cart screen */}
-          <Stack.Screen name="cartScreen" options={{ headerShown: false }} />
+            {/* cart screen */}
+            <Stack.Screen name="cartScreen" options={{ headerShown: false }} />
 
-          {/* checkout screen */}
-          <Stack.Screen name="checkOut" options={{ headerShown: false }} />
+            {/* checkout screen */}
+            <Stack.Screen name="checkOut" options={{ headerShown: false }} />
 
-          {/* success screen */}
-          <Stack.Screen name="success" options={{ headerShown: false }} />
+            {/* success screen */}
+            <Stack.Screen name="success" options={{ headerShown: false }} />
 
-          {/* not found */}
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+            {/* not found */}
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
