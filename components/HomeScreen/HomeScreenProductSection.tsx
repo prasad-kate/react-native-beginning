68aa5b4ad7add0713da1837a05cb4de76f22d40a
prasad-kate <prasadkatepk@gmail.com>
2024-12-11 13:44:32 +0530

    fix: handle empty product list case in home screen products section

--- components/HomeScreen/HomeScreenProductSection.tsx
+++ components/HomeScreen/HomeScreenProductSection.tsx
@@ -1,32 +1,38 @@
 import { useGetProducts } from "@/services/products.service";
 import { homeScreenStyles } from "@/styles/homeScreen.styles";
 import { FlatList } from "react-native";
+import EmptySection from "../DetailsScreens/OrderDetails/OrderDetailsEmptySection";
 import HomeScreenProductCard from "./HomeScreenProductCard";
 
 const HomeScreenProductSection = () => {
   const { productsList } = useGetProducts();
-
   return (
-    <FlatList
-      data={productsList}
-      keyExtractor={(_, index) => `${index}`}
-      renderItem={({ item, index }) => {
-        const isSingleItem =
-          index % 2 === 0 && index === productsList?.length - 1;
-        return (
-          <HomeScreenProductCard
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            price={item.price}
-            isSingleItem={isSingleItem}
-          />
-        );
-      }}
-      columnWrapperStyle={homeScreenStyles.productsContainer}
-      showsVerticalScrollIndicator={false}
-      numColumns={2}
-    />
+    <>
+      {!productsList?.length ? (
+        <EmptySection />
+      ) : (
+        <FlatList
+          data={productsList}
+          keyExtractor={(_, index) => `${index}`}
+          renderItem={({ item, index }) => {
+            const isSingleItem =
+              index % 2 === 0 && index === productsList?.length - 1;
+            return (
+              <HomeScreenProductCard
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                price={item.price}
+                isSingleItem={isSingleItem}
+              />
+            );
+          }}
+          columnWrapperStyle={homeScreenStyles.productsContainer}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+        />
+      )}
+    </>
   );
 };
 
