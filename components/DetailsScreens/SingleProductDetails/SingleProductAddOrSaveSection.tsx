b63426ce994d076d3d3e31e2ac093431fad03693
prasadk <prasadkatepk@gmail.com>
2024-12-17 23:19:12 +0530

    feat: add-to-favourites state management

--- components/DetailsScreens/SingleProductDetails/SingleProductAddOrSaveSection.tsx
+++ components/DetailsScreens/SingleProductDetails/SingleProductAddOrSaveSection.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/ui/Button";
 import useCartStore from "@/store/cartStore";
+import useProductStore from "@/store/productStore";
 import { singleProductScreenStyles } from "@/styles/singleProductScreen.styles";
 import { SingleProductAddOrSaveSectionProps } from "@/types";
 import { Ionicons } from "@expo/vector-icons";
@@ -10,24 +11,44 @@ const SingleProductAddOrSaveSection = ({
   productCount,
 }: SingleProductAddOrSaveSectionProps) => {
   const { id } = useLocalSearchParams();
-  const { setCartItems } = useCartStore();
+  const { cartItems, setCartItems } = useCartStore();
+  const { editFavouriteProducts, favouriteProducts } = useProductStore();
+
+  const isItemAddedToCart = cartItems?.some(
+    (itemData) => itemData?.productId === +id
+  );
+
+  const isItemAddedToFavourites = favouriteProducts?.includes(+id);
+
   return (
     <View style={singleProductScreenStyles.addToCartOrFavouritesContainer}>
       <TouchableOpacity
         activeOpacity={0.9}
         style={singleProductScreenStyles.addToFavouritesButtonContainer}
+        onPress={() => {
+          editFavouriteProducts(+id);
+        }}
       >
-        <Ionicons size={24} name="bookmark" />
+        <Ionicons
+          size={24}
+          name={isItemAddedToFavourites ? "bookmark" : "bookmark-outline"}
+        />
       </TouchableOpacity>
       <Button
-        text="Add To Cart"
-        style={singleProductScreenStyles.addToCartButton}
+        text={isItemAddedToCart ? "Added To Cart" : "Add To Cart"}
+        style={[
+          singleProductScreenStyles.addToCartButton,
+          {
+            opacity: isItemAddedToCart ? 0.5 : 1,
+          },
+        ]}
         onPress={() => {
           setCartItems({
             productId: +id,
             productCount,
           });
         }}
+        disabled={isItemAddedToCart}
       />
     </View>
   );
--- store/productStore.ts
+++ store/productStore.ts
@@ -2,12 +2,32 @@ import { create } from "zustand";
 
 interface ProductState {
   category: number | null;
+  favouriteProducts: number[];
   setCategory: (category: number) => void;
+  editFavouriteProducts: (productId: number) => void;
 }
 
-const useProductStore = create<ProductState>((set) => ({
+const useProductStore = create<ProductState>((set, get) => ({
   category: null,
+  favouriteProducts: [],
   setCategory: (category) => set({ category }),
+  editFavouriteProducts: (productId) => {
+    const { favouriteProducts } = get();
+
+    const isAlreadyFavourite = favouriteProducts?.includes(productId);
+
+    if (!isAlreadyFavourite) {
+      const updatedFavourites = [...favouriteProducts, productId];
+      return set({ favouriteProducts: updatedFavourites });
+    }
+
+    if (isAlreadyFavourite) {
+      const updatedFavourites = favouriteProducts?.filter(
+        (id) => id !== productId
+      );
+      return set({ favouriteProducts: updatedFavourites });
+    }
+  },
 }));
 
 export default useProductStore;
