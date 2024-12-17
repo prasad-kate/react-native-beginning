798fa0180fd43c126ca8d0c56eba87a1c6581af9
prasad-kate <prasadkatepk@gmail.com>
2024-12-17 11:01:14 +0530

    fix: increase api request timeout

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ export const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 2 * 60 * 1000,
   headers: {
     "Content-Type": "application/json",
   },
