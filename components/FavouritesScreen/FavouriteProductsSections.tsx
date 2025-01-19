d091c9b1eeb450ea4fa8b8a8b8b4c92ab01a9f79
prasadk <prasadkatepk@gmail.com>
2025-01-19 19:20:03 +0530

    fix: add empty produts screen at favorites screen when no product is added

--- components/FavouritesScreen/FavouriteProductsSections.tsx
+++ components/FavouritesScreen/FavouriteProductsSections.tsx
@@ -2,6 +2,7 @@ import { useGetFavrouriteProductsDetails } from "@/services/products.service";
 import useCartStore from "@/store/cartStore";
 import { favouriteScreenStyles } from "@/styles/favouriteScreen.styles";
 import { ActivityIndicator, FlatList, View } from "react-native";
+import OrderDetailsEmptySection from "../DetailsScreens/OrderDetails/OrderDetailsEmptySection";
 import Button from "../ui/Button";
 import FavouriteProductCard from "./FavouriteProductCard";
 
@@ -22,38 +23,43 @@ const FavouriteProductsSections = () => {
     <View style={favouriteScreenStyles.sectionContainer}>
       {isAnyPending ? (
         <ActivityIndicator size={"large"} />
+      ) : favouriteProductDetails?.length ? (
+        <>
+          <FlatList
+            data={favouriteProductDetails}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => {
+              return (
+                <FavouriteProductCard
+                  id={item?.id}
+                  name={item?.name}
+                  image={item?.image}
+                  price={item?.price}
+                />
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={favouriteScreenStyles.productsContainer}
+          />
+
+          <Button
+            text="Add all to cart"
+            activeOpacity={0.9}
+            disabled={isAnyPending}
+            style={[
+              favouriteScreenStyles.addAllToCartButton,
+              {
+                opacity: isAnyPending ? 0.5 : 1,
+              },
+            ]}
+            onPress={() => {
+              addMultipleCartItems(addAllProductsToCartPayload);
+            }}
+          />
+        </>
       ) : (
-        <FlatList
-          data={favouriteProductDetails}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => {
-            return (
-              <FavouriteProductCard
-                id={item?.id}
-                name={item?.name}
-                image={item?.image}
-                price={item?.price}
-              />
-            );
-          }}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={favouriteScreenStyles.productsContainer}
-        />
+        <OrderDetailsEmptySection />
       )}
-      <Button
-        text="Add all to cart"
-        activeOpacity={0.9}
-        disabled={isAnyPending}
-        style={[
-          favouriteScreenStyles.addAllToCartButton,
-          {
-            opacity: isAnyPending ? 0.5 : 1,
-          },
-        ]}
-        onPress={() => {
-          addMultipleCartItems(addAllProductsToCartPayload);
-        }}
-      />
     </View>
   );
 };
