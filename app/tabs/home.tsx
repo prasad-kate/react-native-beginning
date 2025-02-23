a9d736d243ff7e052fef4988d4522f636c770481
prasadk <prasadkatepk@gmail.com>
2025-02-23 18:08:42 +0530

    fix: home screen products filter change

--- app/tabs/home.tsx
+++ app/tabs/home.tsx
@@ -4,25 +4,35 @@ import {
   useGetProducts,
   useGetProductsCategories,
 } from "@/services/products.service";
+import { useGlobalSearchStore } from "@/store/globalSearchStore";
 import useProductStore from "@/store/productStore";
 import { homeScreenStyles } from "@/styles/homeScreen.styles";
+import { useMemo } from "react";
 import { ActivityIndicator, View } from "react-native";
 import HomeScreenHeader from "../../components/HomeScreen/HomeScreenHeader";
 
 const HomeScreen = () => {
   const { category: selectedCategory } = useProductStore();
-
+  const { globalSearch } = useGlobalSearchStore();
   const { productCategories, isGettingCategories } = useGetProductsCategories();
   const { productsList, isGettingProducts } = useGetProducts();
 
-  const filteredProducts = productsList?.filter(
-    ({ category_id }: { category_id: number; name: string }) => {
-      if (selectedCategory && selectedCategory !== 1) {
-        return category_id === selectedCategory;
+  const filteredProducts = useMemo(() => {
+    return productsList?.filter(
+      ({ category_id, name }: { category_id: number; name: string }) => {
+        const matchesCategory =
+          selectedCategory && selectedCategory !== 1
+            ? category_id === selectedCategory
+            : true;
+
+        const matchesSearch = globalSearch
+          ? name.toLowerCase().includes(globalSearch.toLowerCase())
+          : true;
+
+        return matchesCategory || matchesSearch;
       }
-      return true; // TODO: shows all products, Later will show popular
-    }
-  );
+    );
+  }, [productsList, selectedCategory, globalSearch]);
 
   return (
     <View style={homeScreenStyles.container}>
